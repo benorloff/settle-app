6d0886a70e7a4f614dec53cb1b61c812b9e637f9
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-05-24 20:33:46 -0700

    new invoice styling

--- src/components/ActiveLineItem/ActiveLineItem.jsx
+++ src/components/ActiveLineItem/ActiveLineItem.jsx
@@ -5,52 +5,59 @@ import { Grid, Form } from 'semantic-ui-react';
 export default function ActiveLineItem({ name, description, rate, quantity, onChange }) {
 
     return(
-        <Grid.Row columns='equal'>
-            <Grid.Column width={10}>
-                <Form.Input
-                    style={{ height: 30 }}
-                    type="text"
-                    name="name"
-                    placeholder="Item Name"
-                    value={name}
-                    onChange={onChange}
-                />
-                <Form.Input
-                    style={{ height: 30 }}
-                    type="text"
-                    name="description"
-                    placeholder="Item Description"
-                    value={description}
-                    onChange={onChange}
-                />
-            </Grid.Column>
-            <Grid.Column width={2} textAlign='right'>
-                <Form.Input
-                    style={{ height: 30 }}
-                    type="number"
-                    name="rate"
-                    value={rate}
-                    onChange={onChange}
-                />
-            </Grid.Column>
-            <Grid.Column width={2} textAlign='right'>
-                <Form.Input
-                    style={{ height: 30 }}
-                    type="number"
-                    name="quantity"
-                    value={quantity}
-                    onChange={onChange}
-                />  
-            </Grid.Column>
-            <Grid.Column width={2} textAlign='right'>
-                <Form.Input
-                    style={{ height: 30 }}
-                    type="number"
-                    name="subtotal"
-                    value={ rate && quantity ? rate * quantity : ''}
-                    onChange={onChange}
-                />
-            </Grid.Column>
-        </Grid.Row>
+        <Grid>
+            <Grid.Row columns='equal'>
+                <Grid.Column width={10}>
+                    <Form.Input
+                        style={{ height: 30 }}
+                        type="text"
+                        name="name"
+                        label="Item Name"
+                        placeholder="Item Name"
+                        value={name}
+                        onChange={onChange}
+                    />
+                    <Form.Input
+                        style={{ height: 30 }}
+                        type="text"
+                        name="description"
+                        label="Item Description"
+                        placeholder="Item Description"
+                        value={description}
+                        onChange={onChange}
+                    />
+                </Grid.Column>
+                <Grid.Column width={2} textAlign='right'>
+                    <Form.Input
+                        style={{ height: 30 }}
+                        type="number"
+                        name="rate"
+                        label="Rate"
+                        value={rate}
+                        onChange={onChange}
+                    />
+                </Grid.Column>
+                <Grid.Column width={2} textAlign='right'>
+                    <Form.Input
+                        style={{ height: 30 }}
+                        type="number"
+                        name="quantity"
+                        label="Qty"
+                        value={quantity}
+                        onChange={onChange}
+                    />  
+                </Grid.Column>
+                <Grid.Column width={2} textAlign='right'>
+                    <Form.Input
+                        style={{ height: 30 }}
+                        type="number"
+                        name="subtotal"
+                        label="Total"
+                        value={ rate && quantity ? rate * quantity : ''}
+                        onChange={onChange}
+                    />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     )
 }
