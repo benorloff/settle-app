89f49af386752bab180b42cb1d736e6b8129d671
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-06-04 17:00:59 -0700

    invoice card formatting

--- src/components/InvoiceCard/InvoiceCard.jsx
+++ src/components/InvoiceCard/InvoiceCard.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Card, Image, Icon, Button, Divider } from 'semantic-ui-react';
+import invoiceApi from '../../utils/invoiceApi';
 
 export default function InvoiceCard({ user, invoice }) {
+
+    const issueDate = new Date(invoice.issueDate)
+    const issueDateStr = issueDate.toLocaleDateString()
+
+    
 
     return (
         <Card 
@@ -17,15 +23,20 @@ export default function InvoiceCard({ user, invoice }) {
                 src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
                 />
                 <Card.Header>{invoice.clientId.firstName} {invoice.clientId.lastName}</Card.Header>
-                <Card.Meta>{invoice.company}</Card.Meta>
+                <Card.Meta>{issueDateStr}</Card.Meta>
                 <Card.Description style={{overflow: 'hidden', textOverflow: 'ellipsis'}}>
-                    {invoice.email}
+                    Invoice #: {invoice.invoiceNum}
                 </Card.Description>
                 { invoice.phone &&       
                     <Card.Description>
                         <Icon name="phone"></Icon> {invoice.phone}
                     </Card.Description>
                 }
+            </Card.Content>
+            <Card.Content>
+                <Card.Header>
+                    {invoice.amountDue}
+                </Card.Header>
             </Card.Content>
         </Card>
     )
