07d2b28f54b07078ecae279b78fc90775df459f9
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-06-06 10:00:23 -0700

    homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Container, Icon, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Container, List, Button, Grid, Segment } from 'semantic-ui-react';
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import Loading from "../../components/Loader/Loader";
 import Header from "../../components/Header/Header";
@@ -34,9 +35,30 @@ export default function Home({ user, handleSignUpOrLogin, handleLogout }) {
     return (
         <>
             <Header user={user} handleLogout={handleLogout} />
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid style={{ height: "100vh", backgroundColor: 'green' }} verticalAlign="middle" centered>
+                <Grid.Row>
+                    <Grid.Column textAlign="center">
+                        <h1>Send invoices. Get paid. Easy.</h1>
+                        { user && 
+                            <Link to="/dashboard"><Button>Dashboard</Button></Link>
+                        }
+                        { !user &&
+                            <Link to="/signup"><Button>Get Started</Button></Link>
+                        }
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            <Segment inverted vertical style={{ padding: '2em 5em' }}>
+                <Container>
+                    <Grid inverted centered>
+                        <Grid.Row>
+                            <Grid.Column>
+                                Copyright Settle. All Rights Reserved.
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+            </Segment>
         </>
     )
 }
